675c936aa627072fd4512f2eccabd9f80d8eeaa0
Limon_Safayet <limon6531@gmail.com>
2020-07-16 21:35:22 +0600

    Passing Data

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pizzas', function () {
+    // get data from db
+    return view('pizzas',['type' => 'maxican pizza']);
+});
